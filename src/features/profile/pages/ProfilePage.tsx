f6505ce89ab1e0a2852d9095a1cc26afd23c051a
rizky a <107133530+rizkyalf11@users.noreply.github.com>
2025-06-12 14:08:39 +0700

    feat: protected auth route

--- src/features/profile/pages/ProfilePage.tsx
+++ src/features/profile/pages/ProfilePage.tsx
@@ -7,6 +7,7 @@ import { Button } from "~/components/ui/button";
 import { Card, CardContent } from "~/components/ui/card";
 import { api } from "~/utils/api";
 import { EditProfileFormInner } from "../components/editProfileFormInner";
+import { AuthRoute } from "~/components/layout/AuthRoute";
 
 const ProfilePage = () => {
   const { data: getProfileData } = api.profile.getProfile.useQuery();
@@ -17,41 +18,43 @@ const ProfilePage = () => {
   };
 
   return (
-    <PageContainer>
-      <SectionContainer padded minFullscreen className="gap-y-6 py-8">
-        <h1 className="text-3xl font-semibold">Profile Settings</h1>
+    <AuthRoute>
+      <PageContainer>
+        <SectionContainer padded minFullscreen className="gap-y-6 py-8">
+          <h1 className="text-3xl font-semibold">Profile Settings</h1>
 
-        <Card className="">
-          <CardContent className="flex gap-6 pt-6">
-            <div className="flex flex-col gap-4">
-              <Avatar className="size-24">
-                <AvatarFallback>VC</AvatarFallback>
-                <AvatarImage />
-              </Avatar>
-              <Button onClick={handleOpenFileExplorel} size="sm">
-                Ganti Foto
-              </Button>
-              <input className="hidden" type="file" ref={inputFileRef} />
-            </div>
+          <Card className="">
+            <CardContent className="flex gap-6 pt-6">
+              <div className="flex flex-col gap-4">
+                <Avatar className="size-24">
+                  <AvatarFallback>VC</AvatarFallback>
+                  <AvatarImage />
+                </Avatar>
+                <Button onClick={handleOpenFileExplorel} size="sm">
+                  Ganti Foto
+                </Button>
+                <input className="hidden" type="file" ref={inputFileRef} />
+              </div>
 
-            <div className="grid flex-1 grid-cols-2 gap-y-4">
-              {getProfileData && (
-                <EditProfileFormInner
-                  defaultValues={{
-                    bio: getProfileData?.bio,
-                    username: getProfileData?.username,
-                  }}
-                />
-              )}
-            </div>
-          </CardContent>
-        </Card>
+              <div className="grid flex-1 grid-cols-2 gap-y-4">
+                {getProfileData && (
+                  <EditProfileFormInner
+                    defaultValues={{
+                      bio: getProfileData?.bio,
+                      username: getProfileData?.username,
+                    }}
+                  />
+                )}
+              </div>
+            </CardContent>
+          </Card>
 
-        <div className="flex justify-end gap-4">
-          <Button>Simpan</Button>
-        </div>
-      </SectionContainer>
-    </PageContainer>
+          <div className="flex justify-end gap-4">
+            <Button>Simpan</Button>
+          </div>
+        </SectionContainer>
+      </PageContainer>
+    </AuthRoute>
   );
 };
 
